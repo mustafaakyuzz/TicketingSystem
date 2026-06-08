369c54436f5bea1c4c2f8eadc8d9a7825588bb6f
Mustafa Akyuz <akyuzmustafaa@hotmail.com>
2026-06-08 18:45:39 +0300

    chore: Swagger documentation added

---
+++ booking-service/src/main/java/com/musti/booking_service/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.musti.booking_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI bookingServiceApi(){
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Booking Service API")
+                        .description("Booking Service API for Musti")
+                        .version("v1.0.0"));
+    }
+}
